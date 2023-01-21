a90b1eae2bad75db73ab9dda0ce0fa2c48edaaa3
angela <yaoliu928@gmail.com>
2023-01-21 14:48:22 +1000

    add some script

--- script.js
+++ script.js
@@ -1,26 +1,52 @@
+console.log('************');
 $(document).ready(function () {
+  var $nav = $("#mainNavbar");
+  var $btn = $("#myBtn");
+  if ($(this).scrollTop() > $nav.height()) {
+    $btn.addClass("active");
+  } else {
+    $btn.removeClass("active");
+  };
   $(document).scroll(function () {
-    var $nav = $("#mainNavbar");
-    $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+    if ($(this).scrollTop() > $nav.height()) {
+      $btn.addClass("active");
+    } else {
+      $btn.removeClass("active");
+    };
   });
+  $("#myBtn").click(function () {
+    $('html, body').animate({
+      scrollTop: $("#home").offset().top
+    }, 100);
+  })
   $("#navHome").click(function () {
     $('html, body').animate({
       scrollTop: $("#home").offset().top
-    }, 1000)
+    }, 100);
   });
   $("#navAbout").click(function () {
     $('html, body').animate({
       scrollTop: $("#about").offset().top
-    }, 1000)
+    }, 100)
+  });
+  $("#moreInfo").click(function () {
+    $('html, body').animate({
+      scrollTop: $("#about").offset().top
+    }, 100)
+  });
+  $("#navActivity").click(function () {
+    $('html, body').animate({
+      scrollTop: $("#activity").offset().top
+    }, 100)
   });
   $("#navPortfolio").click(function () {
     $('html, body').animate({
       scrollTop: $("#portfolio").offset().top
-    }, 1000)
+    }, 100)
   });
   $("#navContact").click(function () {
     $('html, body').animate({
       scrollTop: $("#contact").offset().top
-    }, 1000)
+    }, 100)
   });
 });
